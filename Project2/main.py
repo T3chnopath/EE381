8117af08c791a064d873931710246d276b9ea9e4
Technopath <josias.navasca@gmail.com>
2024-02-28 00:44:08 -0800

    [Feat] Implement enhnaced mode in tx/rx

--- Project2/main.py
+++ Project2/main.py
@@ -1,23 +1,78 @@
 from tx_rx_bit import tx_rx_bit
 
+P0 = 0.35 # Probability of 0
+E0 = 0.01 # Probability of 0 misread
+E1 = 0.02 # Probability of 1 misread
+
+
 def testErrTransmit():
-    pass
+    N = 10_000
+    failure = 0 # Number of times S != R
+    
+    for i in range(N):
+        S, R = tx_rx_bit(P0, E0, E1)
+       
+        if S != R:
+            failure += 1
+    
+    print(f"Probability of tx / rx failure: {failure / N:.5f}")
+
 
 def testR1GivenS1():
-    pass
+    N = 1_000_000
+    total = 0 
+    success = 0 # Number of times S != R
+
+    for i in range(N):
+        S, R = tx_rx_bit(P0, E0, E1)
+       
+        # Detect if R = 1 | S = 1
+        if S:
+            total += 1
+
+            if R:
+                success += 1
+
+    print(f"Probability of R = 1 | S = 1: {(success / total):.5f}")
+
 
 def testS1GivenR1():
-    pass
+    N = 1_000_000
+    total = 0 
+    success = 0 # Number of times S != R
+
+    for i in range(N):
+        S, R = tx_rx_bit(P0, E0, E1)
+       
+        # Detect if S = 1 | R = 1
+        if R:
+            total += 1
+
+            if S:
+                success += 1
+
+    print(f"Probability of S = 1 | R = 1: {(success / total):.5f}")
 
 def testEnhanceTransmit():
-    pass
+    N = 1_000_000
+    failure = 0 # Number of times S != R
+
+    for i in range(N):
+        S, R = tx_rx_bit(P0, E0, E1, True)
+
+        if S != R: 
+            failure += 1
+
+    print(f"Probability to decode incorrectly with TMR: {(failure / N):.3f}")
+
+
 
 # Main function is used for desired test cases
 def main():
     testErrTransmit()
-    # testR1GivenS1()
-    # testS1GivenR1()
-    # testEnhanceTransmit()
+    testR1GivenS1()
+    testS1GivenR1()
+    testEnhanceTransmit()
 
 # Call main if run from the command line
 if __name__ == "__main__":
--- Project2/tx_rx_bit.py
+++ Project2/tx_rx_bit.py
@@ -1,23 +1,35 @@
 import random as rand
 
-def tx_rx_bit(p0, e0, e1):
+def tx_rx_bit(p0, e0, e1, enhanced = False):
+    R_buff = [] 
     M = rand.random() # Used for message chance 
-    T = rand.random() # Used for receive chance
-
+    
     # Set S
     S = True if M <= p0 else False;
 
-    # Set R
-    if   S == False and T <= e0:
-        R = True
-    elif S == False and T > e0:
-        R = False
-    elif S == True and T > e1:
-        R = True
-    elif S == True and T <= e1:
-        R = False
+    # If enhanced transmission, repeat send    
+    if enhanced:
+        txCount = 3
     else:
-        printf("ERROR: R not computed")
+        txCount = 1
 
-    # Return array for user debugging
-    return [S, R]
+    for i in range(txCount):
+        T = rand.random() # Used for receive chance
+        # Set R array
+        if   S == False and T <= e0:
+            R_buff.append(True)
+        elif S == False and T > e0:
+            R_buff.append(False)
+        elif S == True and T > e1:
+            R_buff.append(True)
+        elif S == True and T <= e1:
+            R_buff.append(False)
+        else:
+            print("ERROR: R not computed")
+
+    # If enhanced, perform majority vote
+    if enhanced:
+        return[S, bool(sum(R_buff) > len(R_buff) / 2)]
+    # Otherwise, return first element 
+    else:
+        return [S, R_buff[0]]
